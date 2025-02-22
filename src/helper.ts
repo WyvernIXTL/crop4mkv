97b83833ad83e70e527cba1ca2b7854fd2b8985b
Adam McKellar <dev@mckellar.eu>
2025-02-22 14:30:16 +0100

    docs: added missing license headers

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Adam McKellar <dev@mckellar.eu> 2025
+
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 export function padNumberToString(num: number): string {
     return `${num}`.padStart(2, "0");
 }
